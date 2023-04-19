cc8465c62779e54f2b40f087e299d9aaccfefba0
Altenia Gaines <gaines.tina90@gmail.com>
2023-04-19 16:20:14 -0700

    DOM Nodes

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+let noMain = document.querySelector("main");
+noMain.remove()
+
+const newHeader = document.createElement("h1");
+newHeader.id = 'victory';
+newHeader.textContent = "TINA is the champion"
+
+document.body.append(newHeader);
+
